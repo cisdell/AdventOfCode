f885ffc200b98eb254a324be9b34a135bc37a748
Andrew Cho <cisdell@gmail.com>
2025-12-29 17:46:52 -0500

    work so far

--- day3/solutions.py
+++ day3/solutions.py
@@ -1,13 +1,29 @@
 import os
 
 def solution(data):
+    max_nums = []
     instructions = data.strip().split('\n')
-    pass
+    for line in instructions:
+        line_str = str(line)
+        cur_max = 0
+        for i in range(0,len(line_str)):
+            for j in range(i + 1, len(line_str)):
+                cur_num = int(line_str[i]+line_str[j])
+                cur_max = max(cur_max, cur_num)
+        max_nums.append(cur_max)
+    print(max_nums)
+    return sum(max_nums)
+
+def solution_2(data):
+    max_nums = []
+    instructions = data.strip().split('\n')
+    for line in instructions:
+        line_list = [num for num in str(line)]
 
 
-
-
-
+        max_nums.append(cur_max)
+    print(max_nums)
+    return sum(max_nums)
 
 if __name__ == '__main__':
     base_dir = os.path.dirname(__file__)
@@ -22,4 +38,5 @@ if __name__ == '__main__':
         print(f"File exists: {os.path.exists(file_path)}")
     else:
         result = solution(data)
+
         print(f"Result: {result}")
